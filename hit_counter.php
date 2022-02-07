f773c40fd330d8675dc69cc3280877be98ccced5
josmirpa <71086685+josmirpa@users.noreply.github.com>
2022-02-07 20:01:58 +0100

    Update hit_counter.php

--- hit_counter.php
+++ hit_counter.php
@@ -1,34 +1,34 @@
 <?php
-    $user = "alumno";
-    $password = "Password123#@!";
-    $database = "bbdd";
+    $user = "YOURUSER";
+    $password = "YOURPASSWORD";
+    $database = "YOURDATABASE";
 
     try {
 
         $db = new PDO("mysql:host=mysql;dbname=$database", $user, $password);
 
         $siteVisitsMap  = 'siteStats';
-        $visitorHashKey = '';
+        $visitorKey = '';
 
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 
-           $visitorHashKey = $_SERVER['HTTP_CLIENT_IP'];
+           $visitorKey = $_SERVER['HTTP_CLIENT_IP'];
 
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 
-           $visitorHashKey = $_SERVER['HTTP_X_FORWARDED_FOR'];
+           $visitorKey = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
         } else {
 
-           $visitorHashKey = $_SERVER['REMOTE_ADDR'];
+           $visitorKey = $_SERVER['REMOTE_ADDR'];
         }
 
         $totalVisits = 0;
 
-        $sql="SELECT dir_ip, visitas FROM contador WHERE dir_ip=:dir_ip";
+        $sql="SELECT dir_ip, vis FROM cont WHERE dir_ip=:dir_ip";
 
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':dir_ip', $visitorHashKey);
+        $stmt->bindParam(':dir_ip', $visitorKey);
 
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $result = $stmt->execute();
@@ -36,7 +36,7 @@
         if ($result) {
 
             $row = $stmt->fetch();
-            $totalVisits = $row['visitas'] + 1;
+            $totalVisits = $row['vis'] + 1;
 
         } else {
 
@@ -44,16 +44,16 @@
 
         }
 
-        $sql = "INSERT INTO contador (dir_ip, visitas) VALUES (:dir_ip, :visitas) ON DUPLICATE KEY UPDATE visitas=:visitas";
+        $sql = "INSERT INTO cont (dir_ip, vis) VALUES (:dir_ip, :vis) ON DUPLICATE KEY UPDATE vis=:vis";
 
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':dir_ip', $visitorHashKey);
-        $stmt->bindParam(':visitas', $totalVisits);
+        $stmt->bindParam(':dir_ip', $visitorKey);
+        $stmt->bindParam(':vis', $totalVis);
 
         $stmt->execute();
         
 
-        echo "Bienvenido a un contador de visitas, has visto esta web " .  $totalVisits . " veces\n";
+        echo "Bienvenido a un contador de visitas, has visto esta web " .  $totalVis . " veces\n Empieza a hacer algo productivo \n";
 
     } catch (Exception $e) {
         echo $e->getMessage();
